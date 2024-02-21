6e12fc583a914763a66e70a51f15c2a851eca142
UzziKyle <uzzielkyleynciong0824@gmail.com>
2024-02-21 19:59:06 +0800

    Fix nav tooltip hover

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -20,27 +20,27 @@ export default function Nav() {
 
       {/* larger screens */}
       <ul className="fixed hidden md:flex md:flex-col gap-y-4 w-fit h-screen justify-center items-center shadow-md shadow-black-300">
-        <li className="group relative">
-          <a className="block p-4 hover:drop-shadow-xl" href="/">
+        <li className="relative">
+          <a className="peer block p-4 hover:drop-shadow-xl" href="/">
             <MdHomeFilled />
           </a>
-          <div className="group-hover:opacity-100 absolute top-0 m-4 left-3/4 group-hover:left-full bg-blue-300 px-2 text-white rounded opacity-0 ease-in duration-200">
+          <div className="peer-hover:visible peer-hover:opacity-100 absolute top-0 m-4 left-3/4 peer-hover:left-full bg-blue-300 px-2 text-white rounded invisible opacity-0 ease-in duration-200">
             Home
           </div>
         </li>
-        <li className="group relative">
-          <a className="block p-4 hover:drop-shadow-xl" href="/blog/">
+        <li className="relative">
+          <a className="peer block p-4 hover:drop-shadow-xl" href="/blog/">
             <MdOutlineNoteAlt />
           </a>
-          <div className="group-hover:opacity-100 absolute top-0 m-4 left-3/4 group-hover:left-full bg-blue-300 px-2 text-white rounded opacity-0 ease-in duration-200">
+          <div className="peer-hover:visible peer-hover:opacity-100 absolute top-0 m-4 left-3/4 peer-hover:left-full bg-blue-300 px-2 text-white rounded invisible opacity-0 ease-in duration-200">
             Blog
           </div>
         </li>
-        <li className="group relative">
-          <a className="block p-4 hover:drop-shadow-xl" href="/about/">
+        <li className="relative">
+          <a className="peer block p-4 hover:drop-shadow-xl" href="/about/">
             <MdOutlineFace />
           </a>
-          <div className="group-hover:opacity-100  absolute top-0 m-4 left-3/4 group-hover:left-full bg-blue-300 px-2 text-white rounded opacity-0 ease-in duration-200">
+          <div className="peer-hover:visible peer-hover:opacity-100 absolute top-0 m-4 left-3/4 peer-hover:left-full bg-blue-300 px-2 text-white rounded invisible opacity-0 ease-in duration-200">
             About
           </div>
         </li>
