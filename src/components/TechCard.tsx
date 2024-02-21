a8ede2bcda752635f9e380fda1e0da8842113624
UzziKyle <uzzielkyleynciong0824@gmail.com>
2024-02-21 15:55:23 +0800

    Fix TechCard mobile render; Add picocss

--- src/components/TechCard.tsx
+++ src/components/TechCard.tsx
@@ -1,6 +1,6 @@
 export default function TechCard({ cardData }) {
   return (
-    <div className="group bg-gray-50 relative h-60 rounded-2xl w-[90vw] md:w-[22.5vw] lg:w-[15vw] shadow-md hover:shadow-lg">
+    <div className="group bg-gray-50 relative h-60 rounded-2xl w-[60vw] md:w-[22.5vw] lg:w-[15vw] shadow-md hover:shadow-lg">
       <img
         src={cardData.image}
         className="absolute top-8 h-24 group-hover:-top-5 group-hover:h-32 ease-in-out duration-100 left-0 right-0 m-auto"
---
+++ src/contents/techs/picocss.md
@@ -0,0 +1,5 @@
+---
+image: "https://asset.brandfetch.io/idPvB17D-2/idH-B0-Epv.png"
+name: "Pico CSS"
+description: "CSS Framework"
+---
